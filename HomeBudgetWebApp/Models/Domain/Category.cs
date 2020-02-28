207b2bbb586754ab6294fa50a3c7c8d0f3ea699e
Ariel R <rzeszowsky@gmail.com>
2020-02-28 18:40:56 +0100

    Add Category domain class

---
+++ HomeBudgetWebApp/Models/Domain/Category.cs
@@ -0,0 +1,15 @@
+﻿using HomeBudgetWebApp.Models.Interfaces;
+using System.Collections.Generic;
+
+namespace HomeBudgetWebApp.Models.Domain
+{
+    public class Category : ICategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IEnumerable<Transaction> Transactions { get; set; }
+        public IEnumerable<SubCategory> SubCategories { get; set; }
+
+    }
+}
