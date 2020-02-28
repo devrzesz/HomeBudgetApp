f160ed322bc4e4976c897538fe52d0bc3199fcd0
Ariel R <rzeszowsky@gmail.com>
2020-02-28 18:50:34 +0100

    Add DbContext and corresponding to Domain Classes Entities

---
+++ HomeBudgetWebApp/Models/Entities/BudgetDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using HomeBudgetWebApp.Models.Domain;
+
+namespace HomeBudgetWebApp.Models.Entities
+{
+    public class BudgetDbContext : DbContext
+    {
+        public DbSet<BudgetAccount> budgetAccounts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Payee> Payees{ get; set; }
+        public DbSet<BudgetAccount> budgetAccounts { get; set; }
+
+        public BudgetDbContext()
+            : base("name=DefaultConnection")
+        {
+
+        }
+    }
+}
