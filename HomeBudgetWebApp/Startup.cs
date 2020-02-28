ba4c5085534f876a2145c284e05f477ced013fbb
Ariel R <rzeszowsky@gmail.com>
2020-02-28 15:55:43 +0100

    Dodaj pliki projektów.

---
+++ HomeBudgetWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeBudgetWebApp.Startup))]
+namespace HomeBudgetWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
