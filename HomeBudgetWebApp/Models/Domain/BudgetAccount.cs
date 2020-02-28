fad2570a3e2fc50c4cf91de2828719756f78840d
Ariel R <rzeszowsky@gmail.com>
2020-02-28 18:11:19 +0100

    Add BudgetAccount domain class and BudgetAccountType enum

---
+++ HomeBudgetWebApp/Models/Domain/BudgetAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeBudgetWebApp.Models.Domain
+{
+    public class BudgetAccount
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+        public float OpeningBalance { get; set; }
+        public float Balance { get; set; }
+        public byte Type { get; set; }
+
+        public ICollection<Transaction> Transactions { get; set; }
+    }
+}
---
+++ HomeBudgetWebApp/Models/Enums/BudgetAccountType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeBudgetWebApp.Models.Enums
+{
+    public enum BudgetAccountTypes : byte
+    {
+        Bank = 1,
+        Pocket
+    }
+}
