5da339a852e669d1b8be21e990695bfe2adb5c35
Ariel R <rzeszowsky@gmail.com>
2020-02-28 18:42:55 +0100

    Add using statment in ICategory to access to classes

--- HomeBudgetWebApp/Models/Interfaces/ICategory.cs
+++ HomeBudgetWebApp/Models/Interfaces/ICategory.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using HomeBudgetWebApp.Models.Domain;
 
 namespace HomeBudgetWebApp.Models.Interfaces
 {
