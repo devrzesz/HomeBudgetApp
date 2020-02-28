c7343d152fb8a66d064094dc25624cb7dff07451
Ariel R <rzeszowsky@gmail.com>
2020-02-28 18:45:43 +0100

    Add SubCategory Domain Class

---
+++ HomeBudgetWebApp/Models/Domain/SubCategory.cs
@@ -0,0 +1,16 @@
+﻿using HomeBudgetWebApp.Models.Interfaces;
+using System.Collections.Generic;
+
+namespace HomeBudgetWebApp.Models.Domain
+{
+    public class SubCategory : ICategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MainCategoryId { get; set; }
+
+        public Category MainCategory { get; set; }
+        public IEnumerable<Transaction> Transactions { get; set; }
+
+    }
+}
