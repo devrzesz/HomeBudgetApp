dc634ee211d07f0acb6b2591eebdc1ef92334c9f
Ariel R <rzeszowsky@gmail.com>
2020-02-28 18:39:54 +0100

    Add ICategory interfaces

---
+++ HomeBudgetWebApp/Models/Interfaces/ICategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeBudgetWebApp.Models.Interfaces
+{
+    public interface ICategory
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+
+        IEnumerable<Transaction> Transactions { get; set; }
+        IEnumerable<SubCategory> SubCategories { get; set; }
+    }
+}
